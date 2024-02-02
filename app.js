2217cde638feb86253c91e30b216ddef4acafeea
Joviicam Racoon <joviicam9@gmail.com>
2024-02-01 18:41:05 -0600

    reto cumplido joviis

--- app.js
+++ app.js
@@ -1,24 +1,50 @@
-const db = require('./db/connection');
-const axios = require('axios');
+const db = require("./db/connection");
+const axios = require("axios");
 
 const createClient = () => {
-    axios.get('https://randomuser.me/api/')
-        .then((response) => {
-            const { name } = response.data.results[0];
-            const sql = `INSERT INTO clients (name,last_name, created_at) VALUES ('${name.first}','${name.last}', '2024-02-01 19:07:41')`;
-            db.query(sql, (err, result) => {
-                if (err) throw err;
-                console.log('Cliente creado!');
-                /*const sql = `INSERT INTO logs (description, time_stamp) VALUES ('Cliente creado', NOW())`;
+  axios
+    .get("https://randomuser.me/api/")
+    .then((response) => {
+      const { name } = response.data.results[0];
+      const sql = `INSERT INTO clients (name,last_name, created_at) VALUES ('${name.first}','${name.last}', '2024-02-01 19:07:41')`;
+      db.query(sql, (err, result) => {
+        if (err) throw err;
+        console.log("Cliente creado!");
+        /*const sql = `INSERT INTO logs (description, time_stamp) VALUES ('Cliente creado', NOW())`;
                 db.query(sql, (err, result) => {
                     if (err) throw err;
                     console.log('Log creado!');
                 });*/
-            });
-        })
-        .catch((error) => {
-            console.log(error);
-        });
-}
+      });
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+};
 
-setInterval(createClient,5000);
+//setInterval(createClient, 5000);
+
+const createPet = () => {
+  const types = ["dog", "cat", "bird", "fish", "reptile"];
+  const breeds = [
+    "labrador",
+    "bulldog",
+    "poodle",
+    "siamese",
+    "persian",
+    "parrot",
+    "goldfish",
+    "iguana",
+  ];
+  let names = ["Marlo", "Micket", "Brownie", "Ruggerita", "Pulguita", "Camila"];
+  const type = types[Math.floor(Math.random() * types.length)];
+  const breed = breeds[Math.floor(Math.random() * breeds.length)];
+  const namesPets = names[Math.floor(Math.random() * names.length)];
+  let date = new Date().toISOString().slice(0, 19).replace("T", " ");
+  const sql = `INSERT INTO pets (name, type, breed, owner_id, created_at) VALUES ('${namesPets}','${type}','${breed}', 1, '${date}')`;
+  db.query(sql, (err, result) => {
+    if (err) throw err;
+    console.log("Mascota creada!");
+  });
+};
+setInterval(createPet, 5000);
--- db/connection.js
+++ db/connection.js
@@ -1,16 +1,16 @@
-const mysql = require('mysql2');
+const mysql = require("mysql2");
 
 const connection = mysql.createConnection({
-    host: '127.0.0.1',
-    user: 'root',
-    password: 'admin',
-    database: 'vet',
-    port: '3306',
+  host: "localhost",
+  user: "root",
+  password: "root",
+  database: "vet",
+  port: "3306",
 });
 
 connection.connect((err) => {
-    if (err) throw err;
-    console.log('Conectado a la base de datos!');
+  if (err) throw err;
+  console.log("Conectado a la base de datos!");
 });
 
 module.exports = connection;
